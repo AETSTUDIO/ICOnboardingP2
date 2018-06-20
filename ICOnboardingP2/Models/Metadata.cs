c9839d1292b13a2d71345188c55c6d245f429e20
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-06-20 12:00:46 +1000

    Updated Annotation using Metadata and partial class

---
+++ ICOnboardingP2/Models/Metadata.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ICOnboardingP2.Models
+{
+    public class CustomerMetadata
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name;
+
+        [Required]
+        [StringLength(255)]
+        public string Address;
+
+    }
+
+    public class ProductMetadata
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name;
+
+        [Required]
+        [Range(0.01, 99999.99)]
+        public decimal Price;
+    }
+
+    public class StoreMetadata
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name;
+
+        [Required]
+        [StringLength(255)]
+        public string Address;
+    }
+
+    public class ProductSoldMetadata
+    {
+        [Required]
+        [Display(Name = "Date Sold")]
+        public DateTime DateSold;
+    }
+}
---
+++ ICOnboardingP2/Models/PartialClasses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ICOnboardingP2.Models
+{
+    [MetadataType(typeof(CustomerMetadata))]
+    public partial class Customer
+    {
+    }
+
+    [MetadataType(typeof(ProductMetadata))]
+    public partial class Product
+    {
+    }
+
+    [MetadataType(typeof(StoreMetadata))]
+    public partial class Store
+    {
+    }
+
+    [MetadataType(typeof(ProductSoldMetadata))]
+    public partial class ProductSold
+    {
+    }
+}
