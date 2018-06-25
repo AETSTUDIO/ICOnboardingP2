4de8c77d67c92fbb28c96ec8db7dda8e8297a412
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-06-25 23:02:04 +1000

    Finish Store View Page

---
+++ ICOnboardingP2/Controllers/Api/StoresController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ICOnboardingP2.Models;
+
+namespace ICOnboardingP2.Controllers.Api
+{
+    public class StoresController : ApiController
+    {
+        private OnboardingP2Entities db;
+
+        public StoresController()
+        {
+            db = new OnboardingP2Entities();
+        }
+
+        //GET api/stores
+        public IEnumerable<Store> GetStores()
+        {
+            return db.Stores.ToList();
+        }
+
+        //GET api/stores/id
+        public Store GetStore(int id)
+        {
+            var store = db.Stores.SingleOrDefault(st => st.Id == id);
+
+            if (store == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return store;
+        }
+
+        //POST api/stores
+        [HttpPost]
+        public Store CreateStore(Store store)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            db.Stores.Add(store);
+            db.SaveChanges();
+
+            return store;
+        }
+
+        //PUT /api/stores/1
+        [HttpPut]
+        public void UpdateStore(int id, Store store)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var storeInDb = db.Stores.SingleOrDefault(c => c.Id == id);
+
+            if (storeInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            storeInDb.Name = store.Name;
+            storeInDb.Address = store.Address;
+
+            db.SaveChanges();
+        }
+
+        //DELETE /api/store/id
+        [HttpDelete]
+        public void DeleteStore(int id)
+        {
+            var storeInDb = db.Stores.SingleOrDefault(c => c.Id == id);
+
+            if (storeInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            db.Stores.Remove(storeInDb);
+            db.SaveChanges();
+        }
+    }
+}
